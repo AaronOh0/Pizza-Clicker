62c5fd2d2418fbd76b0653a44314099239446442
Åron <167232717+AaronOh0@users.noreply.github.com>
2024-04-29 20:45:08 +0200

    Add files via upload
    Har kommet lengre

--- Pizza.js
+++ Pizza.js
@@ -8,16 +8,23 @@ let clickerPrice = 25;
 
 let ovens = 0;
 let OvenFarm = 5;
-let ovenPrice = 100;
+let ovenPrice = 150;
+
+let pepperoni = 0;
+let pepperoniMultiplier = 1.5;
+let pepperoniPrice = 500;
 
 function oppstart() {
   document.getElementById("pizza-button").onclick = pizza;
   document.getElementById("clicker-buy").onclick = clickerfarm;
-  document.getElementById("oven-buy").onclick = Ovens;
+  document.getElementById("oven-buy").onclick = BuyOven;
+  document.getElementById("pepperoni-buy").onclick = buyPepperoni;
+  setInterval(addPizzasFromOvens, 1000); // Call addPizzasFromOvens every second
 }
 
 function pizza() {
   pizzas += Math.pow(clickerMultiplier, clicker);
+  pizzas += Math.pow(pepperoniMultiplier, pepperoni);
   document.getElementById("pizzas").innerHTML = pizzas.toFixed(2);
 }
 
@@ -34,4 +41,35 @@ function clickerfarm() {
   }
 }
 
-function Ovens() {}
+function BuyOven() {
+  if (pizzas >= ovenPrice) {
+    ovens++;
+    pizzas -= ovenPrice;
+    ovenPrice = ovenPrice * 1.3;
+    document.getElementById("oven-price").innerHTML = ovenPrice;
+    document.getElementById("oven-quantity").innerHTML = ovens;
+    document.getElementById("pizzas").innerHTML = pizzas.toFixed(2);
+  } else {
+    alert("Du har ikke nokk pizzas for å kjøpe en Oven");
+  }
+}
+
+function addPizzasFromOvens() {
+  pizzas += ovens * 2.5;
+  PizzaprSec = ovens * 2.5;
+  document.getElementById("pizzas").innerHTML = pizzas.toFixed(2);
+  document.getElementById("pizza-per-sec").innerHTML = PizzaprSec;
+}
+
+function buyPepperoni() {
+  if (pizzas >= pepperoniPrice) {
+    pepperoni++;
+    pizzas -= pepperoniPrice;
+    pepperoniPrice = pepperoniPrice * 1.5;
+    document.getElementById("pepperoni-quantity").innerHTML = clicker;
+    document.getElementById("pepperoni-price").innerHTML = pepperoniPrice;
+    document.getElementById("pizzas").innerHTML = pizzas.toFixed(2);
+  } else {
+    alert("Du har ikke nokk pizzas for å kjøpe en pepperoni!");
+  }
+}
